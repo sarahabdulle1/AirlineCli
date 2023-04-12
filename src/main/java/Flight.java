8451e573e5317cd8c25e7c2031848c28037358fa
Subrina7 <Subrinaf.7@gmail.com>
2023-04-12 09:22:59 +0100

    designing methods

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,43 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    String destination;
+    int ID;
+    ArrayList <Passenger> passengers;
+
+    ArrayList <Flight> flights;
+    public Flight(String destination, int ID, ArrayList passengers){
+        this.destination = destination;
+        this.ID = ID;
+        this.passengers = passengers;
+    }
+
+    public String getDestination() {
+        return this.destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return this.passengers;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+
+    //public void addFlight()
+
 }
--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,7 @@
 public class Passenger {
+
+    String name;
+    int ID;
+    String contactInfo;
+
 }
